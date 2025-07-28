3a624af78e397cd4cc1a61236cbb0866442be320
Brian Linton <blintonsideprojects@gmail.com>
2025-07-27 18:30:56 -0700

    frames

--- frameconverter.py
+++ frameconverter.py
@@ -2,19 +2,41 @@ import cv2
 import sys
 import os
 
-def extract_frames(source_file, dest_path):
+def count_frames(source_path):
+  total_frames = 0
+  for root, subdirs, files in os.walk(source_path):
+    for filename in files:
+      fname, file_extension = os.path.splitext(os.path.join(root, filename))
+      if str.lower(file_extension) == ".mp4":
+        vidcap = cv2.VideoCapture(os.path.join(root, filename))
+        total_frames = total_frames + int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+  return total_frames
+
+def extract_frames(source_path, dest_path):
   count = 0
-  success = True
-  vidcap = cv2.VideoCapture(source_file)
-  while success:
-    success,image = vidcap.read()
-    source_file_name = os.path.splitext(os.path.basename(source_file))
-    dest_file_name = source_file_name[0] + "_frame_" + str(count) + ".png"
-    final_dest = os.path.join(dest_path, dest_file_name)
-    cv2.imwrite(final_dest, image)     # save frame as JPEG file      
-    progress_string = "Processing frame #" + str(count) + "\r"
-    sys.stdout.write(progress_string)
-    count = count + 1
+  print ("Counting Frames Please Wait...")
+  total_frames = count_frames(source_path)
+
+  for root, subdirs, files in os.walk(source_path):
+    for filename in files:
+      success = True
+      fname, file_extension = os.path.splitext(os.path.join(root, filename))
+      if str.lower(file_extension) == ".mp4":
+        vidcap = cv2.VideoCapture(os.path.join(root, filename))
+        print (os.path.join(root, filename))
+        if vidcap.isOpened():
+          while success:
+            success,image = vidcap.read()
+            if not success:
+              break
+            source_file_name = os.path.splitext(os.path.basename(filename))
+            dest_file_name = source_file_name[0] + "_frame_" + str(count) + ".png"
+            final_dest = os.path.join(dest_path, dest_file_name)
+            cv2.imwrite(final_dest, image)       
+            progress_string = "Processing frame #" + str(count) + " of " + str(total_frames) + "\r"
+            sys.stdout.write(progress_string)
+            count = count + 1
 
 num_args = len(sys.argv)
 if num_args < 3:
@@ -24,15 +46,4 @@ if num_args < 3:
 source_dir = sys.argv[1]
 dest_dir = sys.argv[2]
 
-for root, subdirs, files in os.walk(source_dir):
-    for filename in files:
-      file_path = os.path.join(root, filename)
-      extract_frames(file_path, dest_dir)
-
-
-#count = 0
-#while success:
-#  cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file      
-#  success,image = vidcap.read()
-#  print('Read a new frame: ', success)
-#  count += 1
+extract_frames(source_dir, dest_dir)
